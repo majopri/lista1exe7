17d063072d64b1e11a9e8e29df302f7509decfa1
User <User@DESKTOP-G0A3IIV>
2024-04-23 00:58:16 -0300

    Adicionar arquivos de projeto.

---
+++ lista1exe7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exe7
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            const double MilhaMaritmaParaMetro = 1852;
+
+            const double MetroParaQuilometro = 0.001;
+
+            Console.WriteLine("Digite a quantidade de milhas marítmas:");
+            double MilhasMaritimas = Convert.ToDouble(Console.ReadLine());
+
+            double quilometros = (MilhasMaritimas *  MilhaMaritmaParaMetro) * MetroParaQuilometro;
+
+            Console.WriteLine($"{MilhasMaritimas} milhas maritimas equivalem a {quilometros} quilometros.");
+        }
+    }
+}
---
+++ lista1exe7/ProgramHelpers.cs
@@ -0,0 +1,31 @@
+﻿using lista1exe7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+static class ProgramHelpers
+{
+    static int Main(string[] args)
+    {
+        const double MilhaMaritmaParaMetro = 1852;
+
+        const double MetroParaQuilometro = 0.001;
+
+        Console.WriteLine("Digite a quantidade de milhas marítmas")
+        }
+        static int Main(string[] args)
+        {
+            const double MilhaMaritmaParaMetro = 1852;
+
+            const double MetroParaQuilometro = 0.001;
+
+            Console.WriteLine("Digite a quantidade de milhas marítmas:");
+            double MilhasMaritimas = Convert.ToDouble(Console.ReadLine());
+
+            double quilometros = (MilhasMaritimas *  MilhaMaritmaParaMetro) * MetroParaQuilometro;
+
+            Console.WriteLine($"{MilhasMaritimas} milhas maritimas equivalem a {quilometros} quilometros.");
+        }
+}
